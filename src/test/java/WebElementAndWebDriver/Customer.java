000f9ff80508bb24dd884dd03bc5edb28d4076da
thaonth1502 <huongthao89@gmail.com>
2024-07-30 06:29:00 +0700

    update

--- src/test/java/WebElementAndWebDriver/Customer.java
+++ src/test/java/WebElementAndWebDriver/Customer.java
@@ -3,7 +3,6 @@ package WebElementAndWebDriver;
 import locators.LocatorsCRM;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
 public class Customer {
